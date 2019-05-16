96da2331d456ad164fd4d11f984c6229d21cd8fe
Denys Mishunov <denys.mishunov@gmail.com>
2019-05-17 01:05:45 +0200

    Retargeting React DOM events
    The retargeting script's author: Lukas Bombach

--- js/frankenstein-wrappers/Header-wrapper.js
+++ js/frankenstein-wrappers/Header-wrapper.js
@@ -1,3 +1,5 @@
+import retargetEvents from "./react-event-retargeting";
+
 // eslint-disable-next-line
 import React from "../../react/node_modules/react";
 /* eslint-disable */
@@ -49,6 +51,7 @@ class FrankensteinWrapper extends HTMLElement {
       </StyleSheetManager>,
       appWrapper
     );
+    retargetEvents(this.shadowRoot);
   }
 }
 
