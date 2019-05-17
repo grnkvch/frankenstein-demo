10375db728c7d707cd15a799761ec0d2a254930c
Denys Mishunov <denys.mishunov@gmail.com>
2019-05-17 03:38:06 +0200

    Styling Header React component

--- config/webpack.conf.js
+++ config/webpack.conf.js
@@ -26,22 +26,53 @@ module.exports = {
         loader: "babel-loader"
       },
       {
-        test: /\.css$/,
-        use: {
-          loader: 'css-loader',
-          options: {
-            modules: true
-          }
-        }
-      },
-      {
         test: /\.(jpg|png|svg|webp)$/,
         use: [
           {
             loader: "file-loader",
             options: {
-              name: "images/[name].[ext]"
+              name: "images/[name].[ext]",
+              publicPath: (url, resourcePath, context) => {
+                if (/react/.test(resourcePath)) {
+                  return `react/public/${url}`;
+                }
+              },
+
             }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        oneOf: [
+          // this matches stylesheets coming from /react/ subfolder
+          {
+            test: /\/react\//,
+            use: [
+              {
+                loader: 'style-loader',
+                options: {
+                  injectType: 'singletonStyleTag',
+                  insert: 'frankenstein-header-wrapper'
+                }
+              },
+              {
+                loader: 'css-loader',
+                options: {
+                  localIdentName: '[local]_[hash:base64:5]'
+                }
+              }
+            ]
+          },
+          {
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  localIdentName: '[local]_[hash:base64:5]'
+                }
+              }
+            ]
           }
         ]
       },
--- js/frankenstein-wrappers/Header-wrapper.js
+++ js/frankenstein-wrappers/Header-wrapper.js
@@ -3,11 +3,52 @@ import React from "../../react/node_modules/react";
 /* eslint-disable */
 import ReactDOM from "../../react/node_modules/react-dom";
 import HeaderApp from "../../react/src/components/Header";
+import { StyleSheetManager } from "../../react/node_modules/styled-components";
+
+import '../../react/node_modules/todomvc-app-css/index.css'
+
+const styleOverrides = '.todoapp{ margin:0; }';
 
 class FrankensteinWrapper extends HTMLElement {
   connectedCallback() {
-    this.attachShadow({ mode: "open" });
-    ReactDOM.render(<HeaderApp />, this.shadowRoot);
+    // In order to not loose the root styles, coming from index.css
+    // we need to set up the wrapping element with .todoapp
+    const appWrapper = document.createElement("div");
+    appWrapper.classList.add("todoapp");
+
+    this.attachShadow({ mode: "open" }).appendChild(appWrapper);
+
+
+    // PULLING IN GLOBAL STYLES AND OVERRIDES:
+    //
+    // 1. Global Styles
+    //
+    // If there are <style> elements in our component waiting to be consumed
+    // by our wrapper, pull those into the Shadow DOM
+    Array.prototype.slice
+      .call(this.querySelectorAll("style"))
+      .forEach(style => {
+        this.shadowRoot.prepend(style);
+      });
+
+    //
+    // 2. Overrides
+    //
+    const overridesStylesheet =  document.createElement("style");
+    overridesStylesheet.innerHTML = styleOverrides;
+    window.requestAnimationFrame(() => {
+      // We do it in the next animation frame for our overrides
+      // to go after Alien's bundled styles
+      this.shadowRoot.appendChild(overridesStylesheet);
+    });
+
+    const target = this.shadowRoot;
+    ReactDOM.render(
+      <StyleSheetManager target={target}>
+        <HeaderApp />
+      </StyleSheetManager>,
+      appWrapper
+    );
   }
 }
 
